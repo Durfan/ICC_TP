0986799d97976ce3b7de736d9463312a213bef30
Durfan <31577607+Durfan@users.noreply.github.com>
2017-12-17 21:47:38 -0200

    update 17,18, visual

--- trabalho.sh
+++ trabalho.sh
@@ -158,6 +158,7 @@ extrai() {
     echo
 
     echo 12: cinco generos com mais titulos
+    echo
     # Separa a coluna 9, troca ',' por '\n' separando as linhas com mais de um item
 	# faz a ordenação e a contagem de itens iguais, retira o item '\N' da contagem
 	# ordena em ordem decrescente e obtem os 5 primeiros itens
@@ -166,24 +167,23 @@ extrai() {
     # cut -f 9 titles.all.tsv | tr ',' '\n' | sort | grep -v '\\N' | uniq -c | sort -n -r | tr -d [:digit:] | tr -d ' ' | head -5 | tee out12
     echo
     
-    echo 13: media das avaliacoes por titulos > \"genero resultado\"
-    # generos=$(cut -f 9 titles.all.tsv | tr ',' '\n' | sort | uniq | grep -v '\\N')
+    echo 13: media das avaliacoes por titulos \> \"genero resultado\"
+    # generos=$(cut -f 9 titles.all.tsv | tr ',' '\n' | sort | grep -v '\\N' | uniq | sort | tr -d [:digit:] | tr -d ' ')
     # for i in $generos;
     # do
     #    item13=$(awk -F"\t" -v pat="$i" '{if ($9 == pat){print $10}}' titles.all.tsv | paste -sd+ - | bc)
     #    total13=$(cut -f 9 titles.all.tsv | tr ',' '\n' | grep -v '\\N' | sort | grep -c $i)
-    #    media13=$(bc <<< "scale=5;$item13 / $total13")
-    #    echo -e $i"\t"$media13
+    #    media13=$(bc <<< "scale=5;$item13/$total13")
+    #    echo -e $i" "$media13 | tee -a out13
     #    item13=
     #    total13=
     #    media13=
     # done
-    echo
 
     echo -e "14: media das avaliacoes por titulos > \"genero resultado\" && numVotes>100"
     echo -e "15: media das avaliacoes por tı́tulos > \"tipo resultado\"\n"
 
-    echo -e 16: razao de titulos com \"runtimeMinutes\" entre 80-120min pelo total
+    echo -e 16: razao de titulos com \"runtimeMinutes\" entre 80\-120min pelo total
     # Atribiu a uma var o valor de linhas encontradas apos selecionar a coluna 8;
     # inverte a busca por "\N" e verificar de a duracao esta em 80-120
     item16a=$(cut -f 8 titles.all.tsv | grep -v "\\N" | awk '$NF >= 80 && $NF <= 120' | wc -l | tr -d ' ')
@@ -191,14 +191,16 @@ extrai() {
     bc <<< "scale=5;$item16a / $item16b" | tee out16
     echo
 
-    echo 17: dez \"Action\" melhor avaliados desde 2005 titleType=movie && numVotes>100
+    echo 17: top 10 \"Action\" melhor avaliados desde 2005
+    echo
 	# Localizar filmes nos devidos parametros, ordenar pela nota dos filmes e extrair os 10 primeiros	
-	awk -F"\t" '{if(($9 ~ /Action/) && ($6 >= 2005 && $6 != "\N") && ($2 ~ /movie/) && ($11 > 100)) print $3,"\t",$10}' title.all.tsv | sort -r -g -t $'\t' -k 2 | head -n 10 | tee out17
+	awk -F"\t" '{if(($9 ~ /Action/) && ($6 >= 2005 && $6 != "\N") && ($2 ~ /movie/) && ($11 > 100)) print $3,"\t",$10}' titles.all.tsv | sort -r -g -t$'\t' -k2 | head -n 10 | tr '\t' ' ' | tee out17
 	echo
 
-    echo 18: cinco \"Comedy\" melhor avaliados com runtimeMinutes>200min
+    echo 18: top 5 \"Comedy\" melhor avaliados
+    echo
 	# Localizar filmes nos devidos parametros, ordenar pela nota dos filmes e extrair os 5 primeiros	
-	awk -F"\t" '{ if (($2 ~ /movie/) && ($9 ~ /Comedy/) && ($11 > 100) && ($8 > 200)) print $3,"\t",$10}' titles.all.tsv | sort -r -g -t $'\t' -k 2 | head -n 5 | tee out18
+	awk -F"\t" '{ if (($2 ~ /movie/) && ($9 ~ /Comedy/) && ($11 > 100) && ($8 > 200)) print $3,"\t",$10}' titles.all.tsv | sort -r -g -t$'\t' -k2 |  head -n 5 | tr '\t' ' ' | tee out18
     echo
 
     cd ~-
@@ -225,7 +227,7 @@ extrai10() {
     for i in $anos;
         do
             item10=$(cut -f 6 titles.all.tsv | grep -c "$i")
-            media10=$(bc <<< "scale=5;$item10 / $range")
+            media10=$(bc <<< "scale=5;$item10/$range")
             echo -e $i"\t"$media10 | tee -a out10
             item10=
         done
@@ -245,7 +247,6 @@ imprime() {
     echo
     echo -------- Item 1
     cat out1
-    echo ---------------
     echo
     read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
     clear
@@ -261,9 +262,32 @@ imprime() {
     echo -n " Item 9: " ; cat out9
     echo -n "Item 11: " ; cat out11
     echo -n "Item 16: " ; cat out16
-    cd ~-
     echo
     read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
+    clear
+    echo -e $LOGO
+    echo
+    echo -------- Item 12
+    cat out12
+    echo
+    read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
+    clear
+    echo -e $LOGO
+    echo
+    echo -------- Item 17
+    echo
+    cat out17
+    echo
+    read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
+    clear
+    echo -e $LOGO
+    echo
+    echo -------- Item 18
+    echo
+    cat out18
+    echo
+    read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
+    cd ~-
 }
 
 
